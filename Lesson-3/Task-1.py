865de9ed9a7119736596280b1754484b797e6890
Ilona <kasparovailona@gmail.com>
2021-12-14 00:39:52 +0500

    Update Task-1.py

--- Lesson-3/Task-1.py
+++ Lesson-3/Task-1.py
@@ -8,3 +8,11 @@
 # Сделайте это разными способами:
 # 1. с помощью трех команд print
 # 2. с помощью одной команды print
+
+# Вариант 1
+print ('Glo Academy')
+print ('')
+print ('Glo Academy')
+
+# Вариант 2
+print ('Glo Academy', 'Glo Academy', sep='\n\n')
