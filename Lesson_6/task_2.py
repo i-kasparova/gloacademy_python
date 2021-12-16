10f5369f10f42d91813b5cf61d67ca5c31bfb2af
Ilona <kasparovailona@gmail.com>
2021-12-17 04:51:45 +0500

    Create task_2.py

---
+++ Lesson_6/task_2.py
@@ -0,0 +1,8 @@
+# Вам дается текст из некоторого поста. Вам нужно узнать, есть ли в этом посте упоминание об Glo Academy.
+
+text = input()
+
+if 'Glo Academy' in text:
+    print('YES')
+else:
+    print('NO')
