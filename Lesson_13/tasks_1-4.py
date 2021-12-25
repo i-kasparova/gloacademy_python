3de23154cac9610f2df59449c6e676add825e948
Ilona <kasparovailona@gmail.com>
2021-12-25 11:48:07 +0500

    Update tasks_1-4.py

--- Lesson_13/tasks_1-4.py
+++ Lesson_13/tasks_1-4.py
@@ -2,15 +2,17 @@
 
 from random import randint
 
-def is_valid_user(user_input):
+
+def is_valid_user(user_input, max):
     if user_input.isdigit():
         user_number = int(user_input)
-        if user_number > 1 and user_number < 100:
+        if user_number >= 1 and user_number <= max:
             return True
         else:
             return False
     else:
         return False
+
 
 def is_valid_max(max):
     if max.isdigit():
@@ -18,26 +20,37 @@ def is_valid_max(max):
     else:
         return False
 
+
+def is_valid_again(yesno):
+    if yesno.isdigit():
+        user_answer = int(yesno)
+        if user_answer == 1 or user_answer == 2:
+            return True
+        else:
+            return False
+    else:
+        return False
+
 print('Добро пожаловать!')
 
 while True:
-    
+
     while True:
         print('До какого числа загадывать?')
         max = input()
         if is_valid_max(max):
+            max = int(max)
             break
 
     secret_number = randint(1, int(max))
-    # print(secret_number)
     attempts = 0
 
     while True:
         user_input = input(f'Введите число от 1 до {max}: ')
 
-        if not is_valid_user(user_input):
+        if not is_valid_user(user_input, max):
             continue
-        
+
         user_number = int(user_input)
 
         if secret_number > user_number:
@@ -52,7 +65,24 @@ while True:
             print(f'Количество попыток: {attempts}')
             break
 
-    print('Сыграем еще?')
+    while True:
+        print('Сыграем еще? 1 -- Да, 2 -- Нет')
+        onceagain = (input())
+
+        if not is_valid_again(onceagain):
+            continue
+
+        yesno = int(onceagain)
+        print(yesno)
+        if yesno == 1:
+            willwecontinue = 1
+            break
+        else:
+            willwecontinue = 0
+            break
+    
+    if not willwecontinue:
+        break
     
 # Оптимальным алгоритмом является бинарный поиск, т.е. деление пополам, поэтому начинать следует с середины
 # В случае с поиском 22 из 100:
